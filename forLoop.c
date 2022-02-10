768300af1d09134236b3d34f0fbcb943e6606045
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-02-10 13:18:47 +0600

    initial commit

---
+++ forLoop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    // print your name ten time
+    for (int count = 0; count < 10; count++)
+    {
+        printf("Md Mohibulla Sheikh \n");
+    }
+
+    return 0;
+}
