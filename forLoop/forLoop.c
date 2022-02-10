bc5aac3a4f434a1ad69d4cee09726701f8c70bbf
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-02-10 13:56:53 +0600

    adding sum of even number from 0 to 10

--- forLoop/forLoop.c
+++ forLoop/forLoop.c
@@ -8,5 +8,16 @@ int main()
         printf("Md Mohibulla Sheikh \n");
     }
 
+    // print the sum of even number from 1 to 10;
+    int sum = 0;
+    for (int numberCounter = 0; numberCounter <= 10; numberCounter++)
+    {
+        if (numberCounter % 2 == 0)
+        {
+            sum += numberCounter;
+        }
+    }
+    printf("Sum of event number from 1 to 10 is %d", sum);
+
     return 0;
 }
